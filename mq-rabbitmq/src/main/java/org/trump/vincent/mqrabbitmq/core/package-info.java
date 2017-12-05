c5eebf705d1db76a2763691e3aff515f5fe04912
vincent <vincent_unique@163.com>
2017-12-05 18:06:03 +0800

    modify the module mq-rabbitmq

--- mq-rabbitmq/src/main/java/org/trump/vincent/mqrabbitmq/core/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Vincent on 2017/12/5 0005.
- */
-package org.trump.vincent.mqrabbitmq.core;
---
+++ mq-rabbitmq/src/main/java/org/trump/vincent/rabbitmq/core/producer/MessageSender.java
@@ -0,0 +1,7 @@
+package org.trump.vincent.rabbitmq.core.producer;
+
+/**
+ * Created by Vincent on 2017/12/5 0005.
+ */
+public class MessageSender {
+}
