b8649cc8fa8658de1c5b428c1efd83eac21dfd6b
vincent <vincent_unique@163.com>
2017-12-11 22:05:14 +0800

    new feature for asynchronous io

---
+++ io-socket/src/main/java/org/trump/vincent/io_socket/aio/tcp/callback/AsynchronousTcpServer.java
@@ -0,0 +1,6 @@
+package org.trump.vincent.io_socket.aio.tcp.callback;
+
+
+public class AsynchronousTcpServer {
+
+}
---
+++ io-socket/src/main/java/org/trump/vincent/io_socket/aio/tcp/future/AsynchronousTcpServer.java
@@ -0,0 +1,9 @@
+package org.trump.vincent.io_socket.aio.tcp.future;
+
+/**
+ * AIO — Socket
+ * @Reference https://www.ibm.com/developerworks/cn/java/j-lo-nio2/index.html
+ */
+public class AsynchronousTcpServer {
+
+}
