52fdb6bacdba37c56846ba37395a74ce04d742fd
vincent <vincent_unique@163.com>
2017-12-13 09:32:22 +0800

    update for memcached module

--- nosql-memcached/src/main/java/org/trump/vincent/memcached/MemcachedConnection.java
+++ nosql-memcached/src/main/java/org/trump/vincent/memcached/MemcachedConnection.java
@@ -1,7 +1,14 @@
 package org.trump.vincent.memcached;
 
+import net.spy.memcached.MemcachedClient;
+
 /**
  * Created by Vincent on 2017/12/8 0008.
  */
 public class MemcachedConnection {
+    private volatile static MemcachedClient memcachedClient;
+    public static String MEMCACHED_HOST = "127.0.0.1";
+    public static Integer MEMCACHED_PORT = 11211;
+
+
 }
