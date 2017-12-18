3a680664100733a468d5f7c512c5e5a58698c3d4
vincent <vincent_unique@163.com>
2017-12-18 23:04:01 +0800

    update for bio in io-socket

--- io-socket/src/main/java/org/trump/vincent/io_socket/bio/tcp/TcpClient.java
+++ io-socket/src/main/java/org/trump/vincent/io_socket/bio/tcp/TcpClient.java
@@ -1,11 +1,16 @@
 package org.trump.vincent.io_socket.bio.tcp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.CharBuffer;
 
 public class TcpClient {
     private Socket socket;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     public TcpClient(String host,Integer port){
         try {
@@ -22,17 +27,34 @@ public class TcpClient {
      * @param <T>
      * @throws IOException
      */
-    public <T> void run(T request,String response)throws IOException{
+    public <T> void run(T request,String response){
         String result = "";
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        writer.write(request.toString());
-        writer.close();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        CharBuffer buffer = CharBuffer.allocate(1024);
-        while (reader.read(buffer)!=-1){
-            result += buffer.toString();
-            buffer.clear();
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            writer.write(request.toString());
+            writer.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            CharBuffer buffer = CharBuffer.allocate(1024);
+            while (reader.read(buffer) != -1) {
+                result += buffer.toString();
+                buffer.clear();
+            }
+            response = result;
+
+            /**
+             * Final task ,close the stream.
+             */
+            {
+                writer.close();
+                reader.close();
+
+                /**
+                 * socket closing is called by TCP client rather than TCP Server( SocketServer）
+                 */
+                socket.close();
+            }
+        }catch (IOException e){
+            logger.error("Exception occurs in emitting the request or fetching the response.");
         }
-        response = result;
     }
 }
--- io-socket/src/main/java/org/trump/vincent/io_socket/bio/tcp/TcpServer.java
+++ io-socket/src/main/java/org/trump/vincent/io_socket/bio/tcp/TcpServer.java
@@ -1,5 +1,8 @@
 package org.trump.vincent.io_socket.bio.tcp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,6 +12,9 @@ public class TcpServer {
 
     private ServerSocket server;
     private Semaphore semaphore = new Semaphore(20);
+
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
     public TcpServer(Integer port){
         try {
             server = new ServerSocket(port);
@@ -17,14 +23,35 @@ public class TcpServer {
         }
     }
 
-    public void run(CallBack callBack)throws IOException{
+    /**
+     * Blocking-wait-style for new socket connetcion
+     * @param callBack
+     * @throws IOException
+     */
+    @Deprecated
+    public void syncRun(CallBack callBack)throws IOException{
+        while (true){
+            logger.info("New accept for new socket connection.");
+            Socket socket = server.accept();
+            logger.info("Main handler for socket request.");
+            callBack.task(socket);
+        }
+    }
+
+    /**
+     * New-thread-style to handle new socket request
+     * @param callBack
+     * @throws IOException
+     */
+    public void asyncRun(CallBack callBack)throws IOException{
         while (true){
             Socket socket = server.accept();
-
+            logger.info("Handler of new thread style for new socket connection.");
             new Thread(){
                public void run(){
                    try {
                        semaphore.acquire();
+                       logger.info("Thread["+Thread.currentThread().getId()+"] is handling the socket request.");
                        callBack.task(socket);
                    }catch (InterruptedException e){
                        e.printStackTrace();
