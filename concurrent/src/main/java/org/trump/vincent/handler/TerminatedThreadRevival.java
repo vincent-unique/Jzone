8cb707113364e8f020ad731d33517b5671d067ba
vincent <vincent_unique@163.com>
2017-12-21 18:45:59 +0800

    perfect strategy to revive the thread which is terminated by uncaught exception

---
+++ concurrent/src/main/java/org/trump/vincent/handler/TerminatedThreadRevival.java
@@ -0,0 +1,59 @@
+package org.trump.vincent.handler;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Vincent on 2017/12/21 0021.
+ */
+
+/**
+ * Revival Strategy for terminated thread by UncaughtException (RuntimeException)
+ * @Reference http://www.importnew.com/14434.html
+ */
+public class TerminatedThreadRevival {
+
+    private Thread worker;
+
+    public void loop() {
+        if (this.worker == null) {
+            System.out.println("The worker null and will be rebuild.");
+            this.worker = new CoreTask();
+            this.worker.start();
+        }
+    }
+
+    class WorkUncaughtExceptionHander implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            e.printStackTrace();
+            worker = new CoreTask();
+            worker.start();
+        }
+    }
+
+    public class CoreTask extends Thread {
+
+        private final AtomicInteger counter = new AtomicInteger(0);
+
+        public void run() {
+            Thread.setDefaultUncaughtExceptionHandler(new WorkUncaughtExceptionHander());
+            while (true) {
+                System.out.println("Counter:" + counter.get());
+                if (counter.getAndIncrement() / 5 == 2) {
+                    throw new RuntimeException("Thread RuntimeException.");
+                } else {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new TerminatedThreadRevival().loop();
+    }
+}
---
+++ io-socket/src/main/java/org/trump/vincent/netty/rpc/thread/RPCThreadPool.java
@@ -0,0 +1,7 @@
+package org.trump.vincent.netty.rpc.thread;
+
+/**
+ * Created by Vincent on 2017/12/20 0020.
+ */
+public class RPCThreadPool {
+}
