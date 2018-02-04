0081379f786028b7e4d64b1197b5bec6aa2ed9c0
vincent <vincent_unique@163.com>
2018-02-04 16:13:17 +0800

    add K-sum algorithm

---
+++ algorithm/src/main/java/org/trump/vincent/algorithm/leetcode/array/Ksum.java
@@ -0,0 +1,38 @@
+package org.trump.vincent.algorithm.leetcode.array;
+
+/**
+ * K-Sum 问题是一类经典的问题（2-Sum、3-Sum、4-Sum。。。）
+ *
+ * K-Sum问题，最直接的方法是暴力破解方式（时间复杂度是 n的k次方）；
+ * 该问题的算法优化是，将时间复杂度降一个维度
+ */
+public class Ksum {
+
+    /**
+     * Problem_1:
+     * Desc: 从整形数组中找出一组数字（两个数），其和值为给定值（sumNum）
+     *
+     * @param nums
+     * @param sumNum
+     * @return
+     */
+    public static int[] towSum(int[] nums, int sumNum){
+
+        return null;
+    }
+
+    /**
+     * Problem_1:
+     * Desc: 从整形数组中找出一组数字（3个数），其和值为给定值（sumNum）
+     *
+     * @param nums
+     * @param sumNum
+     * @return
+     */
+    public static int[] threeSum(int[] nums, int sumNum){
+
+        return null;
+    }
+
+
+}
